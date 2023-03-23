e24232b236b1cac161b1411e7690ed704fe9477b
delsix <delsixreborn@gmail.com>
2023-03-23 21:51:08 +0200

    first commit

---
+++ dispatcher/src/main/java/ua/delsix/controller/TelegramBot.java
@@ -0,0 +1,24 @@
+package ua.delsix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Controller
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
