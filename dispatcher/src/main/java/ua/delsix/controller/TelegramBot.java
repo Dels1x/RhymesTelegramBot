c02feb6ebcef8262d8dac4eb58c530a24ea71552
delsix <delsixreborn@gmail.com>
2023-03-23 22:34:42 +0200

    Day 1: simple echo bot

--- dispatcher/src/main/java/ua/delsix/controller/TelegramBot.java
+++ dispatcher/src/main/java/ua/delsix/controller/TelegramBot.java
@@ -1,24 +1,54 @@
 package ua.delsix.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-@Controller
+@Component
+@Log4j
 public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
 
     @Override
     public String getBotUsername() {
-        return null;
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        var message = update.getMessage();
+        String text = message.getText();
+        Long chatId = message.getChatId();
 
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText(text);
+        sendMessage.setChatId(chatId);
+
+        sendMessage(sendMessage);
+    }
+
+    public void sendMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+                log.debug(message.getText());
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
---
+++ dispatcher/src/main/java/ua/delsix/controller/UpdateController.java
@@ -0,0 +1,7 @@
+package ua.delsix.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UpdateController {
+}
