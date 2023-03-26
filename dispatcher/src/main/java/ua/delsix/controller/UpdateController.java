bde494a6bcb02a91af792c6077b9bdc285aa5403
delsix <delsixreborn@gmail.com>
2023-03-26 14:43:38 +0300

    Day 3: More refactoring

--- dispatcher/src/main/java/ua/delsix/controller/UpdateController.java
+++ dispatcher/src/main/java/ua/delsix/controller/UpdateController.java
@@ -3,12 +3,13 @@ package ua.delsix.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ua.delsix.service.impl.DatamuseServiceImpl;
 import ua.delsix.utils.MessageUtils;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @Log4j2
@@ -33,26 +34,14 @@ public class UpdateController {
             String answer;
 
             if (text != null) {
-                if(text.equals("/start")) {
+                if (text.equals("/start")) {
                     answer = "Hello there! This bot will help you find rhymes to a word you want.\n" +
                             "Just type in the word, and the bot will find rhymes for you.\n\nNotice that it doesn't work " +
                             "with words that don't exist in an actual English dictionary.";
 
                     //Checking if message consists only of latin letters
                 } else if (text.matches("^[a-zA-Z]+$")) {
-                    // Get last word from a string, in case if user wrote multiple, to give a rhyme for the last word
-                    String lastWord = text.split(" ")[text.split(" ").length - 1];
-
-                    List<String> rhymes = datamuseService.getPerfectRhymes(lastWord);
-                    rhymes.addAll(datamuseService.getNearRhymes(lastWord));
-                    if(rhymes.size() > 0) {
-                        answer = String.format("Rhymes to a word \"%s\":\n\n%s",
-                                        message.getText().toLowerCase(),
-                                        rhymes.stream().collect(Collectors.joining(", ")));
-                    } else {
-                        answer = "Unfortunately, couldn't manage to find any rhymes." +
-                                " Make sure the word is spelled correctly and exists in English";
-                    }
+                    answer = findRhymes(text);
                 } else {
                     answer = "Your message must consist only of latin letters and nothing else.";
                 }
@@ -64,8 +53,26 @@ public class UpdateController {
             log.debug(String.format("UpdateController - processUpdate: message \"%s\"" +
                     " sent to MessageUtils - generateSendMessage()", answer));
 
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             log.error("Error in UpdateController - processUpdate()", e);
+        }
+
+    }
+
+    public String findRhymes(String text) throws IOException {
+        // Get last word from a string, in case if user wrote multiple, to give a rhyme for the last word
+        String lastWord = text.split(" ")[text.split(" ").length - 1];
+
+        List<String> rhymes = datamuseService.getPerfectRhymes(lastWord);
+        rhymes.addAll(datamuseService.getNearRhymes(lastWord));
+        if (rhymes.size() > 0) {
+            return String.format("Rhymes to a word \"%s\":\n\n%s",
+                    text.toLowerCase(),
+                    String.join(", ", rhymes));
+        } else {
+            return "Unfortunately, couldn't manage to find any rhymes." +
+                    " Make sure the word is spelled correctly and exists in English";
         }
     }
 }
