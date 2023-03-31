ff3805e33a7fd91d1d51e7c461a393370013834a
delsix <delsixreborn@gmail.com>
2023-03-31 18:34:38 +0300

    Day 6: Docker and RabbitMQ implemented. RabbitMQ receives messages through queues successfully

---
+++ dispatcher/src/main/java/ua/delsix/configuration/RabbitConfiguration.java
@@ -0,0 +1,27 @@
+package ua.delsix.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static ua.delsix.RabbitQueue.*;
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue MESSAGE_UPDATE() {
+        return new Queue(MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue ANSWER_UPDATE() {
+        return new Queue(ANSWER_UPDATE);
+    }
+}
--- dispatcher/src/main/java/ua/delsix/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ua/delsix/service/impl/UpdateProducerImpl.java
@@ -1,6 +1,7 @@
 package ua.delsix.service.impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ua.delsix.service.UpdateProducer;
@@ -8,9 +9,15 @@ import ua.delsix.service.UpdateProducer;
 @Service
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug("UpdateProducer - produce(): "+ update.getMessage().getText());
-
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
