949503a6459b102eb0f29e3d973614e52ed9af13
delsix <delsixreborn@gmail.com>
2023-04-05 19:59:22 +0300

    Day 10: building jar file

--- dispatcher/src/main/java/ua/delsix/DispatcherApplication.java
+++ dispatcher/src/main/java/ua/delsix/DispatcherApplication.java
@@ -1,11 +1,15 @@
 package ua.delsix;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j
 public class DispatcherApplication {
     public static void main(String[] args) {
+        System.out.println("hello world 1");
+        log.debug("helo world 2");
         SpringApplication.run(DispatcherApplication.class);
     }
 }
